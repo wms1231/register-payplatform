e3881c1bb8f7dfd335afd25f1f30ddd8928acf69
wms1231 <77218328@qq.com>
2017-11-01 09:04:23 +0800

    去了空格；

--- src/main/java/com/bsoft/register/service/impl/SendMessageServiceImpl.java
+++ src/main/java/com/bsoft/register/service/impl/SendMessageServiceImpl.java
@@ -1,15 +1,11 @@
 package com.bsoft.register.service.impl;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import javax.annotation.Resource;
-
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
-
 import com.bsoft.client.schema.sms.MessageFormat;
 import com.bsoft.client.schema.sms.SendMethodType;
 import com.bsoft.client.schema.sms.SendSmsRequest;
